1f0931235b36090801902e10899a6848040d7d1f
Danil Terentev <danil.terentyev@gmail.com>
2024-10-13 15:55:50 +0300

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -8,41 +8,46 @@ api = '...'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+@dp.message_handler(text=["/start"])
+async def start_message(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
 
 class UserState(StatesGroup):
-
     age = State()
     growth = State()
     weight = State()
 
 
-    @dp.message_handler(text = 'Calories')
-    async def set_age(message):
-        await message.answer('Введите свой возраст.')
-        await UserState.age.set()
+@dp.message_handler(text = 'Calories')
+async def set_age(message):
+    await message.answer('Введите свой возраст.')
+    await UserState.age.set()
 
 
-    @dp.message_handler(state = UserState.age)
-    async def set_growth(message, state):
-        await state.update_data(age = message.text)
-        await message.answer(f'Введите свой рост')
-        await UserState.growth.set()
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer(f'Введите свой рост')
+    await UserState.growth.set()
 
 
-    @dp.message_handler(state = UserState.growth)
-    async def set_weight(message, state):
-        await state.update_data(growth = message.text)
-        await message.answer(f'Введите свой вес')
-        await UserState.weight.set()
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer(f'Введите свой вес')
+    await UserState.weight.set()
 
-    @dp.message_handler(state = UserState.weight)
-    async def send_calories(message, state):
-        data = state.get_data()
-        await state.update_data(weight = message.text)
-        data = state.get_data()
-        calories = 10 * weight + 6.25 * growth - 5 * age + 5
-        await state.finish()
-        await message.answer(f'Ваша норма калорий: {calories}')
+@dp.message_handler(state = UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+    await message.answer(f'Ваша норма калорий: {calories}')
+    await state.finish()
 
-    if __name__ == '__main__':
-        executor.start_polling(dp,skip_updates=True)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
